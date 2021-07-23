89f6343321e4758893407cbbd3058c4790d0c994
Nikita Serba <nikitaserba@icloud.com>
2021-07-24 02:14:40 +0300

    Initial commit

---
+++ tgstats/main.cpp
@@ -0,0 +1,34 @@
+/**
+ *  _____ _____  _____ _        _
+ * |_   _|  __ \/  ___| |      | |
+ *   | | | |  \/\ `--.| |_ __ _| |_ ___
+ *   | | | | __  `--. \ __/ _` | __/ __|
+ *   | | | |_\ \/\__/ / || (_| | |_\__ \
+ *   \_/  \____/\____/ \__\__,_|\__|___/
+ *
+ * Statistics for telegram chats, including private messages.
+ * Copyright (C) 2021 Nikita Serba. All rights reserved.
+ * https://github.com/sandsbit/TGStats/
+ *
+ *  This file is part of TGStats.
+ *
+ *  TGStats is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as
+ *  published by the Free Software Foundation, either version 3 of
+ *  the License, or (at your option) any later version.
+ *
+ *  TGStats is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with ec8pp. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
